e3c314c64cd1a1ae0fbb7831d263fa9af56f705f
mohd-asim <108861147+mohd-asim@users.noreply.github.com>
2024-11-21 12:34:44 +0530

    Add CandidateRepository.java extending MongoRepository for Candidate model

---
+++ src/main/java/com/mirai/repository/CandidateRepository.java
@@ -0,0 +1,9 @@
+package com.mirai.repository;
+
+import com.mirai.model.Candidate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateRepository extends MongoRepository<Candidate, String> {
+}
