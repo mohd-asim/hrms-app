d949c0f9e8026dead08f79a32a5d6b21fdd24d95
mohd-asim <108861147+mohd-asim@users.noreply.github.com>
2024-11-21 12:34:36 +0530

    Add main application class MiraiApplication.java

---
+++ src/main/java/com/mirai/MiraiApplication.java
@@ -0,0 +1,12 @@
+package com.mirai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiraiApplication.class, args);
+    }
+}
